82adfc3cbde4ba011cffba7cb0d6f3041b894d3f
juno <junho0618@gitauto.com>
2019-02-14 16:18:44 +0900

    enable CoRoutine

--- Inc/FreeRTOSConfig.h
+++ Inc/FreeRTOSConfig.h
@@ -97,7 +97,7 @@
 #define configUSE_PREEMPTION                     1
 #define configSUPPORT_STATIC_ALLOCATION          0
 #define configSUPPORT_DYNAMIC_ALLOCATION         1
-#define configUSE_IDLE_HOOK                      0
+#define configUSE_IDLE_HOOK                      1
 #define configUSE_TICK_HOOK                      0
 #define configCPU_CLOCK_HZ                       ( SystemCoreClock )
 #define configTICK_RATE_HZ                       ((TickType_t)1000)
@@ -114,7 +114,7 @@
 #define configUSE_PORT_OPTIMISED_TASK_SELECTION  1
 
 /* Co-routine definitions. */
-#define configUSE_CO_ROUTINES                    0
+#define configUSE_CO_ROUTINES                    1
 #define configMAX_CO_ROUTINE_PRIORITIES          ( 2 )
 
 /* Set the following definitions to 1 to include the API function, or zero
